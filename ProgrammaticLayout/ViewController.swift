223115cc7db445c28046a21bfacc59e194689deb
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2024-03-27 02:15:05 +0300

    Initial Commit

---
+++ ProgrammaticLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammaticLayout
+//
+//  Created by furkan sakız on 27.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
