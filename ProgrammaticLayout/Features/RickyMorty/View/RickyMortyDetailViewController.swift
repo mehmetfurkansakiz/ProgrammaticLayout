a46d54e4d9798772712ddee77893442673064e09
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2024-04-08 04:18:52 +0300

    Added RickyMortyDetail.

---
+++ ProgrammaticLayout/Features/RickyMorty/View/RickyMortyDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RickyMortyDetailViewController.swift
+//  ProgrammaticLayout
+//
+//  Created by furkan sakız on 8.04.2024.
+//
+
+import UIKit
+
+class RickyMortyDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
